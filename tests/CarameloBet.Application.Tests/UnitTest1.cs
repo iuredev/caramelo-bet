aa2b782db5e800216da2fb302d96c6b7ab280c0b
Iure Silva <iure.sgz@gmail.com>
2026-04-24 18:54:19 -0300

    chore: initialize solution structure and docker compose

---
+++ tests/CarameloBet.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CarameloBet.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/CarameloBet.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CarameloBet.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
